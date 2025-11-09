cfdfc9d0e8a539251d8c4cc62d21c7859aaf411b
Tusnelde LS Endjala <tusneldelainae@gmail.com>
2025-11-09 11:46:45 +0200

    Task 2: Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,21 +1,9 @@
-import ProfilePage from './components/ProfilePage';
-import UserContext from './context/UserContext';
-import './App.css'
+import ProfilePage from './ProfilePage';
 
 function App() {
-  const userData = { 
-    name: "Jane Doe", 
-    email: "jane.doe@example.com" 
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <div>
-        <h1 style={{ textAlign: 'center' }}>User Profile with Context API</h1>
-        <ProfilePage />
-      </div>
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
